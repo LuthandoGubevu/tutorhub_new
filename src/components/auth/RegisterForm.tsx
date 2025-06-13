bf2a4aaaaac7c378ed3ad60cd785c27898627139
Lutando Gubevu <TidoTeez@gmail.com>
2025-06-13 09:02:09 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/auth/RegisterForm.tsx
+++ src/components/auth/RegisterForm.tsx
@@ -3,7 +3,7 @@
 "use client";
 
 import { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form'; // Added Controller here
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import { Button } from '@/components/ui/button';
